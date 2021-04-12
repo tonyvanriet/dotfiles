88ebdc835e2e0c7edc3f9ff357a24235a7ed6251
Tony van Riet <tony.van.riet@nfiindustries.com>
2021-04-12 08:53:38 -0500

    Just put aliases in config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,31 @@
-source ~/.config/fish/aliases.fish
+alias c="cd"
+funcsave c
+alias h="history"
+funcsave h
+alias cr="clear"
+funcsave cr
+
+# mix
+alias mff="mix format --check-formatted"
+funcsave mff
+alias mfe="mix format --check-equivalent"
+funcsave mfe
+alias mb="mix bless"
+funcsave mb
+alias mfb="mix format --check-equivalent; and mix bless"
+funcsave mfb
+alias mcc="mix do clean, compile"
+funcsave mcc
+alias mch="mix coveralls.html"
+
+function iet --description 'iex in test mode'
+  set -lx MIX_ENV test
+  iex $argv
+end
+
+function gcn --description 'clone a repo from NFIBrokerage'
+  git clone git@github.com:NFIBrokerage/$argv
+end
 
 # idempotently add to global fish_user_paths
 # can use `fish_add_path` once I bump to fish 3.2
@@ -55,7 +82,7 @@ function gho
   end
 end
 
-function purge --description 'Delete all local branches that are already merged to current branch (excludes master)'
-  command git branch --merged | grep -v "\*" | grep -v "master" | xargs -n 1 git branch -d
+function purge --description 'Delete all local branches that are already merged to current branch (excludes main and master)'
+  command git branch --merged | grep -v "\*" | grep -v "master" | grep -v "main" | xargs -n 1 git branch -d
   command git remote prune origin
 end
